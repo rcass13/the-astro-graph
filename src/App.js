1c4452f2248348a9dd5f5c2f06967117f3d2bb28
RoseCass <rosecassidy1303@gmail.com>
2023-02-07 20:01:21 -0800

    login and create account page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
+// import Login from './components/Login';
+// import CreateAccount from './components/CreateAccount';
 
-
+import Login from './pages/Login';
 
 const App = () => (
     <BrowserRouter>
         <Box>
             <Routes>
                 {/* example <Route path="/" exact element={ <Frontpage />}/> */}
-                <Route/>
-                <Route/>
+                <Route path="/Login" exact element ={<Login/>}/>
+                {/* <Route path="/Login" exact element ={<CreateAccount/>}/> */}
                 <Route/>
             </Routes>
         </Box>
---
+++ src/components/CustomInput.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function CustomInput(props) {
+    const errorMsg = props.error ? <p>{props.error}</p> : null;
+    return (
+        <div >
+            <label htmlFor="name">{props.label}:</label>
+            <input
+                type={props.type}
+                id={props.name}
+                value={props.value}
+                onChange={(e) => props.onChange(e.target.value)}
+            />
+            {errorMsg}
+      </div>
+    );
+}
+
+export default CustomInput;
