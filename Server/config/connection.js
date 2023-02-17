3e4a6ffeda28297a6fbf18e5ae4afbba80445a96
Aecop <joonruiz08@gmail.com>
2023-02-16 19:46:09 -0800

    Heroku deploy

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -4,16 +4,21 @@ require('dotenv').config();
 
 console.log(process.env)
 
-const sequelize = new Sequelize(
+let sequelize;
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    "Cool",
+    "CutMeinfor10%!",
     {
       host: '127.0.0.1',
       dialect: 'mysql',
       port: 3306
     }
   );
+}
 
 
 module.exports = sequelize;
