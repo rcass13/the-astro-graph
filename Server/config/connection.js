ef31631a08773daa18930cfeeeec446768afb8c9
RoseCass <rosecassidy1303@gmail.com>
2023-02-21 22:07:34 -0800

    deployment

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -7,7 +7,7 @@ console.log(process.env)
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    "Cool",
+    "BettyTwi",
     {
       host: '127.0.0.1',
       dialect: 'mysql',
